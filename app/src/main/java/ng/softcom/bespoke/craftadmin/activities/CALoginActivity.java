e87cc6779307b9c5362a81fb5343a402fb35963a
Omonayajo Oladapo <o.omonayajo@gmail.com>
2016-06-09 03:27:40 +0100

    did some Code refactoring

--- app/src/main/java/ng/softcom/bespoke/craftadmin/activities/CALoginActivity.java
+++ app/src/main/java/ng/softcom/bespoke/craftadmin/activities/CALoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -101,7 +100,6 @@ public class CALoginActivity extends AppCompatActivity {
                                     CAAlerts.showLongToast(CALoginActivity.this, apiResponse.getMessage());
                                 }
                             } else {
-                                Log.d(TAG, "LOGIN RESPONSE: " + response.toString());
                                 loginFailed();
                             }
                         }
@@ -111,7 +109,6 @@ public class CALoginActivity extends AppCompatActivity {
                             progressBar.setVisibility(View.GONE);
 
                             loginFailed();
-                            Log.d(TAG, "CALogin Failure: " + t.getMessage());
                             t.printStackTrace();
                         }
                     });
